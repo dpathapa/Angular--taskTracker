d813bdcd1038dacd2df2a6f53afee80396527f83
Deepa Thapa <dthapa7606@gmail.com>
2021-09-12 22:29:07 +0100

    Added model and task services

---
+++ taskTracker/src/app/services/task.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { TASKS } from '../mock-task';
+import { Task } from '../Task';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor() { }
+
+  // getTasks(){
+  //   return TASKS;
+  // }
+
+  //using Observables
+  getTasks(): Observable<Task[]>{
+    const tasks = of(TASKS);
+    return tasks;
+  }
+}
